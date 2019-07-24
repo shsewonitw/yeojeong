9a2e9b55a98ceeced07de9007f166932ff0f295a
jb-Moon <zergcow@hanmail.net>
2019-07-24 11:55:08 +0900

    dao,model

---
+++ src/main/java/com/tje/yeojeong/service/MessageCountBySenderService.java
@@ -0,0 +1,34 @@
+package com.tje.yeojeong.service;
+
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tje.yeojeong.model.*;
+import com.tje.yeojeong.repository.*;
+import com.tje.yeojeong.setting.*;
+
+@Service
+public class MessageCountBySenderService {
+	
+	@Autowired
+	private MessageDAO messageDAO;
+	@Autowired
+	private PagingInfo pagingInfo;
+	
+	public Object service(Object args) {
+		int totalCount = 
+				messageDAO.selectBySenderCount((Message)args);
+		int totalPageCount = 
+				totalCount / pagingInfo.getPagingSize() +
+				(totalCount % pagingInfo.getPagingSize() == 0 ? 0 : 1);
+		
+		HashMap<String, Integer> result = 
+				new HashMap<String, Integer>();
+		result.putIfAbsent("totalCount", totalCount);
+		result.putIfAbsent("totalPageCount", totalPageCount);
+		
+		return result;
+	}
+}

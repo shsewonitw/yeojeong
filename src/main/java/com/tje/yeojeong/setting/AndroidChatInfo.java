eaf731ad52c4427bbaec6d261a17f0cee7f06848
shsewonitw <shsewonitw@gmail.com>
2019-09-16 00:12:14 +0900

    915

--- src/main/java/com/tje/yeojeong/setting/AndroidChatInfo.java
+++ src/main/java/com/tje/yeojeong/setting/AndroidChatInfo.java
@@ -1,5 +1,20 @@
 package com.tje.yeojeong.setting;
 
-public class AndroidChatInfo {
+import java.util.ArrayList;
 
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.ApplicationScope;
+
+@ApplicationScope
+@Component
+public class AndroidChatInfo {
+	
+	ArrayList<String> chatList = new ArrayList<String>();
+	
+	public AndroidChatInfo() {}
+	
+	public ArrayList<String> getChatList() {
+		return chatList;
+		
+	}
 }
